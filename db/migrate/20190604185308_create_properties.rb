18c7dbac07b35435094497d2ee85c0ad7333c667
Rebecca Strauss <rebecca@Rebeccas-MBP.lan1>
2019-06-04 15:10:34 -0400

    Added model Associations for Users & Property. CreateProperties migration. Added some seed data.

---
+++ db/migrate/20190604185308_create_properties.rb
@@ -0,0 +1,13 @@
+class CreateProperties < ActiveRecord::Migration[5.2]
+  def change
+    create_table :properties do |t|
+      t.string :address
+      t.string :state
+      t.integer :zipcode
+      t.integer :bedrooms
+      t.float :bathrooms
+      t.integer :max_guests
+      t.string :description
+    end
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+properties = Property.create([
+  {address: '2736 Merrick Avenue', state: 'New York', zipcode: 11566, bedrooms: 5, bathrooms: 2.5, max_guests: 10, description: "Family Home. Has a Pool and billards table."},
+  {address: '13720 Gulf Blvd', state: 'Florida', zip: 33708, bedrooms: 3, bathrooms: 2, max_guests: 7, description: "Beach front rental. Right across from the famous Candy Kitchen!"}
+])
